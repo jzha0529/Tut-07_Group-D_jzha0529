a69bb8f15e5f2d1a69a245ae481939267df0b153
oop12301 <jzha0529@uni.sydney.edu.au>
2024-05-25 20:52:46 +1000

    decrease some line weight when playing the audio

--- sketch.js
+++ sketch.js
@@ -21,7 +21,7 @@ function setup() {
   buttonPos();
   button.mousePressed(play_pause);
 
-  //createSlider() from p5.js, for controling the audio volume.
+  //createSlider() from p5.js.org, create a slider in here for controling the audio volume.
   slider = createSlider(0, 1, 0.5, 0.1);
   sliderPos();
 }
@@ -67,38 +67,40 @@ function createLines()
   strokeCap(SQUARE);
   //scale the canvas.
   let scaleFactor = min(windowWidth/1920, windowHeight/1080);
-  let lineHeight = windowHeight / windowWidth;
   scale(scaleFactor);
 
   let spectrum = fft.analyze();//get the audio frequency
   let sum = 0;
   for(let i = 0; i < spectrum.length; i++)
   {
-    sum += spectrum[i];//plus 
+    sum += spectrum[i];
   }
   let frequency = sum / spectrum.length;//get the average of the frequency.
-  let audioLength = map(frequency, 0, 150, 1.0, 1.5);//map it to 1.0, 1.5 to control the line.
+  let audioLength = map(frequency, 0, 150, 1, 1.7);//map it to 1.0, 1.5 to control the line.
+  //set 1 so it won't change the original line shape. 
+  //1.7 = larger this value, the greater the change in line length.
 
+  let audioLength2 = map(frequency, 0, 150, 1, 0.001);//decrease the property when playing the audio.
   
   //radians angle mode value
   //attach the audioLength to the length property of lines.
   let lineSettings = [//array for each set of lines
-    { x: 0.27, y: 0.44, angle: -0.58, color: 0, weight: 1, lengthLeft: 0.28 * audioLength * lineHeight, lengthRight: 0.20 * audioLength, distanceStart: -0.05, distanceEnd: 0.03, num: 20 },
+    { x: 0.27, y: 0.44, angle: -0.58, color: 0, weight: 1, lengthLeft: 0.28 * audioLength, lengthRight: 0.20 * audioLength, distanceStart: -0.05, distanceEnd: 0.03, num: 20 },
     { x: 0.59, y: 0.39, angle: -0.58, color: 0, weight: 1, lengthLeft: 0.76 * audioLength, lengthRight: 0.76 * audioLength, distanceStart: -0.06, distanceEnd: 0.02, num: 20 },
     { x: 0.30, y: 0.50, angle: -0.58, color: 0, weight: 1, lengthLeft: 0.03 * audioLength, lengthRight: 0.03 * audioLength, distanceStart: -0.07, distanceEnd: 0.05, num: 40 },
     { x: 0.34, y: 0.56, angle: -0.58, color: 0, weight: 1, lengthLeft: 0.26 * audioLength, lengthRight: 0.22 * audioLength, distanceStart: -0.13, distanceEnd: -0.10, num: 14 },
-    { x: 0.48, y: 0.45, angle: -0.58, color: 0, weight: 10, lengthLeft: 0.30 * audioLength, lengthRight: 0.28 * audioLength, distanceStart: -0.13, distanceEnd: -0.12, num: 1 },
-    { x: 0.78, y: 0.24, angle: -0.58, color: 0, weight: 10, lengthLeft: 0.14 * audioLength, lengthRight: 0.15 * audioLength, distanceStart: -0.14, distanceEnd: -0.12, num: 1 },
-    { x: 0.68, y: 0.32, angle: -0.58, color: 0, weight: 10, lengthLeft: 0.05 * audioLength, lengthRight: 0.08 * audioLength, distanceStart: -0.15, distanceEnd: -0.11, num: 2 },
+    { x: 0.48, y: 0.45, angle: -0.58, color: 0, weight: 10 * audioLength2, lengthLeft: 0.30, lengthRight: 0.28, distanceStart: -0.13, distanceEnd: -0.12, num: 1 },
+    { x: 0.78, y: 0.24, angle: -0.58, color: 0, weight: 10 * audioLength2, lengthLeft: 0.14, lengthRight: 0.15, distanceStart: -0.14, distanceEnd: -0.12, num: 1 },
+    { x: 0.68, y: 0.32, angle: -0.58, color: 0, weight: 10 * audioLength2, lengthLeft: 0.05, lengthRight: 0.08, distanceStart: -0.15, distanceEnd: -0.11, num: 2 },
     { x: 0.69, y: 0.31, angle: -0.58, color: 0, weight: 1, lengthLeft: 0.04 * audioLength, lengthRight: 0.29 * audioLength, distanceStart: -0.18, distanceEnd: 0.01, num: 40},
     { x: 0.68, y: 0.44, angle: -0.58, color: 0, weight: 1, lengthLeft: 0.76 * audioLength, lengthRight: 0.76 * audioLength, distanceStart: -0.06, distanceEnd: 0.02, num: 20},
-    { x: 0.78, y: 0.375, angle: -0.58, color: 0, weight: 2, lengthLeft: 0.3 * audioLength, lengthRight: 0.39 * audioLength, distanceStart: -0.06, distanceEnd: 0.02, num: 20},
-    { x: 0.70, y: 0.58, angle: -0.58, color: 0, weight: 6, lengthLeft: 0.60 * audioLength, lengthRight: 0.60 * audioLength, distanceStart: -0.15, distanceEnd: -0.15, num: 1},
-    { x: 0.48, y: 0.53, angle: -0.58, color: 0, weight: 2, lengthLeft: 0.33 * audioLength, lengthRight: 0.33 * audioLength, distanceStart: -0.025, distanceEnd: 0.01, num: 10},
-    { x: 0.78, y: 0.44, angle: -0.58, color: 0, weight: 6, lengthLeft: 0.20 * audioLength, lengthRight: 0.20 * audioLength, distanceStart: -0.15, distanceEnd: -0.15, num: 1},
+    { x: 0.78, y: 0.375, angle: -0.58, color: 0, weight: 2 * audioLength2, lengthLeft: 0.3 * audioLength, lengthRight: 0.39 * audioLength, distanceStart: -0.06, distanceEnd: 0.02, num: 20},
+    { x: 0.70, y: 0.58, angle: -0.58, color: 0, weight: 6 * audioLength2, lengthLeft: 0.60, lengthRight: 0.60, distanceStart: -0.15, distanceEnd: -0.15, num: 1},
+    { x: 0.48, y: 0.53, angle: -0.58, color: 0, weight: 2 * audioLength2, lengthLeft: 0.33 * audioLength, lengthRight: 0.33 * audioLength, distanceStart: -0.025, distanceEnd: 0.01, num: 10},
+    { x: 0.78, y: 0.44, angle: -0.58, color: 0, weight: 6 * audioLength2, lengthLeft: 0.20, lengthRight: 0.20, distanceStart: -0.15, distanceEnd: -0.15, num: 1},
     { x: 0.48, y: 0.45, angle: -0.58, color: 0, weight: 1, lengthLeft: 0.53 * audioLength, lengthRight: 0.53 * audioLength, distanceStart: -0.13, distanceEnd: -0.11, num: 1 },
-    { x: 0.45, y: 0.64, angle: -0.58, color: 0, weight: 6, lengthLeft: 0.20 * audioLength, lengthRight: 0.20 * audioLength, distanceStart: -0.15, distanceEnd: -0.15, num: 1},
-    { x: 0.30, y: 0.56, angle: -0.58, color: 0, weight: 4, lengthLeft: 0.20 * audioLength, lengthRight: 0.20 * audioLength, distanceStart: -0.15, distanceEnd: -0.08, num: 1},
+    { x: 0.45, y: 0.64, angle: -0.58, color: 0, weight: 6 * audioLength2, lengthLeft: 0.20, lengthRight: 0.20, distanceStart: -0.15, distanceEnd: -0.15, num: 1},
+    { x: 0.30, y: 0.56, angle: -0.58, color: 0, weight: 4 * audioLength2, lengthLeft: 0.20, lengthRight: 0.20, distanceStart: -0.15, distanceEnd: -0.08, num: 1},
   ];
   for (let settings of lineSettings) {//Iteration
     drawLines(settings);//Call drawLines() to draw the lines
@@ -122,6 +124,7 @@ function drawLines({ x, y, angle, color, weight, lengthLeft, lengthRight, distan
   pop();
 }
 
+//create a function for play/pause audio and loop it.
 function play_pause()
 {
   if(song.isPlaying())
